45694f720f8778d2a901a811ac2844df17d11e1c
Ben M <42870961+melansob@users.noreply.github.com>
2019-05-10 11:00:12 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,41 @@
+/* Nim Simple */
+
+//initalize variables
+var winner="";
+const turnsParam=3;
+main(); 
+
+//start game
+main(){
+  alert("welcome to NIM, you're first");
+  let count=0;
+  while(count<21){
+    count = playerTurn(count);
+    alert("Current count is "+count);
+    if (count<21) {
+      cpuTurn();
+      alert("Current count is "+count);
+    }
+  }
+  alert(winner+" won!");
+}
+
+function playerTurn(count){
+  let playerInput=0;
+  while (playerInput<1 || playerInput>turnsParam) {
+    playerInput=parseInt(prompt("count how many? (1-"+turnsParam+")"));
+  }
+  count+=playerInput;
+  winner="CPU";
+}
+
+function cpuTurn(){
+  let cpuInput=Math.floor(Math.random()*turnsParam+1);
+  if (count+cpuInput >= 21) {
+    cpuInput=1;
+  }
+  alert("CPU counts "+cpuInput);
+  count+=cpuInput;
+  winner="you";
+  return count;
+}
